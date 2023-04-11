13fc4f3a284e039508fb39af51aba8f1fa8fa420
Fpsska <fpsska1337@gmail.com>
2023-04-11 13:09:57 +0300

    init: initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+// middleware
+
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send("START route");
+});
+
+// connection
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
